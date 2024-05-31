107626ff3beefbf5c040f120ec395f15a554c69b
PRINCE <princekofasiedu@gmail.com>
2024-05-31 09:42:11 +0000

    style: modify design

--- resources/views/signup.blade.php
+++ resources/views/signup.blade.php
@@ -1,15 +1,15 @@
 @component('app')
     <section class="flex flex-col gap-2 justify-center items-center bg-white rounded-md p-1">
         <div class="w-[500px] .h-[700px]">
-            <div class="mt-24 mb-14">
-                <h2 class="text-xl font-bold text-gray-700">RepCollect</h2>
+            <div class="mt-24 mb-14 flex items-center gap-2">
+                <h2 class="text-xl font-bold text-gray-700">RepCollect</h2> | <span>Sign Up</span>
             </div>
 
             <div>
                 <h1 class="text-2xl font-bold text-gray-700">Get Started Now</h1>
                 <p class="mb-10">Enter your credentials to access your account</p>
 
-                <a href="#google-login" class="border border-slate-300 p-1 rounded-md flex gap-3 items-center justify-center font-bold text-sm text-gray-700">
+                <a href="{{route('auth.google')}}" class="border border-slate-300 p-1 rounded-md flex gap-3 items-center justify-center font-bold text-sm text-gray-700">
                 <svg xmlns="http://www.w3.org/2000/svg" width="25" height="26" viewBox="0 0 25 26" fill="none">
                 <path d="M23.5 13.2607C23.5 12.4482 23.4271 11.667 23.2917 10.917H12.5V15.3493H18.6667C18.401 16.7816 17.5938 17.9951 16.3802 18.8076V21.6826H20.0833C22.25 19.6878 23.5 16.7503 23.5 13.2607Z" fill="#4285F4"/>
                 <path d="M12.5003 24.458C15.5941 24.458 18.1878 23.432 20.0837 21.682L16.3805 18.807C15.3545 19.4945 14.042 19.9007 12.5003 19.9007C9.51595 19.9007 6.98991 17.8851 6.08887 15.1768H2.26074V18.1455C4.14616 21.8903 8.02116 24.458 12.5003 24.458Z" fill="#34A853"/>
