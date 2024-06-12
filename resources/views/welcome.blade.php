fd808b8c974bc65d4685a3e0d8da9e6ee69e0cc8
PRINCE <princekofasiedu@gmail.com>
2024-06-12 18:59:11 +0000

    feat: link home create-a-link to focus form

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -27,7 +27,7 @@
         <div class="w-[500px] my-[40px] ">
             <p class="text-gray-700 font-bold flex items-center gap-2">Steps <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1.75" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-hash"><line x1="4" x2="20" y1="9" y2="9"/><line x1="4" x2="20" y1="15" y2="15"/><line x1="10" x2="8" y1="3" y2="21"/><line x1="16" x2="14" y1="3" y2="21"/></svg>  </p>
             <ol class=" mx-[20px]">
-                <li class="flex gap-2">1. Create a link <a href="#" class="bg-blue-400 text-white text-sm p-1 rounded-md w-[120px] flex gap-2 items-center justify-center">
+                <li class="flex gap-2">1. Create a link <a href="{{route('me.collect')}}" class="bg-blue-400 text-white text-sm p-1 rounded-md w-[120px] flex gap-2 items-center justify-center">
                     <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-link"><path d="M10 13a5 5 0 0 0 7.54.54l3-3a5 5 0 0 0-7.07-7.07l-1.72 1.71"/><path d="M14 11a5 5 0 0 0-7.54-.54l-3 3a5 5 0 0 0 7.07 7.07l1.71-1.71"/></svg>
                     Create a link</a> </li>
                 <li class="flex gap-5 relative">2. Fill in details
