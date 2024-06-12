b56fa74a63d79bf941026ca535b875c58006b421
PRINCE <princekofasiedu@gmail.com>
2024-06-12 18:58:43 +0000

    style: add loader to focus form

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,23 @@ export default {
         "./resources/**/*.vue",
     ],
     theme: {
-        extend: {},
+        extend: {
+            animation: {
+                move: "move 1s ease-in-out infinite",
+                rotate: "rotate 1s ease-in-out infinite",
+            },
+            keyframes: {
+                rotate: {
+                    "100%": {
+                        transform: "translateX(-50%) rotate(360deg)",
+                    },
+                },
+                move: {
+                    "0%": { transform: "rotate(10deg)" },
+                    "100%": { transform: "rotate(-10deg)" },
+                },
+            },
+        },
     },
     plugins: [],
 };
