53c1dc9c2d85dcaea8cb10a7eeef830fe0e11d9d
PRINCE <princekofasiedu@gmail.com>
2024-06-18 18:02:10 +0000

    db: create faqs table

--- database/migrations/2024_06_17_141835_create_faqs_table.php
+++ database/migrations/2024_06_17_141835_create_faqs_table.php
@@ -13,6 +13,9 @@ return new class extends Migration
     {
         Schema::create('faqs', function (Blueprint $table) {
             $table->id();
+            $table->text('question');
+            $table->text('answer')->nullable();
+            $table->boolean('approved')->default(false);
             $table->timestamps();
         });
     }
