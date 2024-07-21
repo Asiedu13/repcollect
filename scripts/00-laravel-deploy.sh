e92cc68d0ec142a011763beec6d3176c38622486
Prince Asiedu <princekofasiedu@gmail.com>
2024-07-21 13:58:44 +0000

    deploy: update laravel deploy script

--- scripts/00-laravel-deploy.sh
+++ scripts/00-laravel-deploy.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
-# echo "Running composer"
-# composer global require hirak/prestissimo
+echo "Running composer"
+composer global require hirak/prestissimo
 
-php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
-php composer-setup.php --install-dir=/usr/local/bin --filename=composer
-php -r "unlink('composer-setup.php');"
+# php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
+# php composer-setup.php --install-dir=/usr/local/bin --filename=composer
+# php -r "unlink('composer-setup.php');"
 
 # composer install --no-dev --working-dir=/var/www/html
 composer install --no-dev --optimize-autoloader
