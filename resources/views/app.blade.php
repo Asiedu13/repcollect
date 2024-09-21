6dc721d02f2d199dd3e8c6828cb5dc3cabbaaa92
Prince Asiedu <princekofasiedu@gmail.com>
2024-09-21 16:13:12 +0000

    fix: hamburger menu dead

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -10,7 +10,7 @@
 <body class="bg-gray-100 mx-[100px] min-h-full">
      <header class="flex justify-between py-[10px]">
         <nav>
-            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-align-justify"><line x1="3" x2="21" y1="6" y2="6"/><line x1="3" x2="21" y1="12" y2="12"/><line x1="3" x2="21" y1="18" y2="18"/></svg>
+            <!-- <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-align-justify"><line x1="3" x2="21" y1="6" y2="6"/><line x1="3" x2="21" y1="12" y2="12"/><line x1="3" x2="21" y1="18" y2="18"/></svg> -->
         </nav>
 
         <a href="<?php echo route('home') ?>">
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -11,7 +11,7 @@
 <body class="bg-gray-100 mx-[100px]">
     <header class="flex justify-between .text-center py-[10px]">
         <nav>
-            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-align-justify"><line x1="3" x2="21" y1="6" y2="6"/><line x1="3" x2="21" y1="12" y2="12"/><line x1="3" x2="21" y1="18" y2="18"/></svg>
+            <!-- <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-align-justify"><line x1="3" x2="21" y1="6" y2="6"/><line x1="3" x2="21" y1="12" y2="12"/><line x1="3" x2="21" y1="18" y2="18"/></svg> -->
         </nav>
 
      <a href="<?php echo route('home') ?>">
