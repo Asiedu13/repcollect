f840a20ebb81d4decfd844f7da31802be5e2f09e
Prince Asiedu <princekofasiedu@gmail.com>
2024-09-23 04:04:16 +0000

    style: logo edit

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -6,17 +6,17 @@
         @vite('resources/css/app.css')
         <title>{{ $title ?? 'Page Title' }}</title>
     </head>
-    <body class="bg-gray-100 mx-[100px] min-h-full">
-     <header class="flex justify-between py-[10px] h-[100px]">
+    <body class="bg-gray-100 .mx-[100px] min-h-full">
+     <header class="flex justify-between py-[10px] px-[100px] h-[80px] border-b sticky top-0 bg-slate-600 z-40">
         <nav class="flex justify-center items-center">
             {{-- <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-align-justify"><line x1="3" x2="21" y1="6" y2="6"/><line x1="3" x2="21" y1="12" y2="12"/><line x1="3" x2="21" y1="18" y2="18"/></svg> --}}
              <a href="<?php echo route('home') ?>">
-                <h2 class="text-xl font-bold">RepCollect</h2>
-            </a>
+             <h2 class="text-2xl font-bold text-slate-200">RepCollect</h2>
+             </a>
         </nav>
         @guest
         <a href="<?php echo route('home') ?>" class='flex gap-2 justify-center items-center'>
-            <h2 class="text-xl font-bold">RepCollect</h2>
+        <h2 class="text-2xl font-bold text-slate-600">RepCollect</h2>
         </a>
         @endguest
         @auth
