6dad2169d2ab9739dc2637b80ea6f593241d38fa
Prince Asiedu <princekofasiedu@gmail.com>
2024-09-23 03:56:13 +0000

    feat: add a footer

---
+++ resources/views/livewire/footer.blade.php
@@ -0,0 +1,38 @@
+<footer class="bg-slate-600 text-slate-200 min-w-screen h-[350px] p-[100px] mt-40 grid grid-cols-2">
+            <section>
+                <h2 class="text-xl font-medium">RepCollect</h2>
+                <p class="w-3/4 mt-3">
+                    Where leaders, business owners and representatives can 
+                    streamline the process of collecting payments for goods, group projects, class resources and more.
+                </p>
+                <livewire:socials />
+                <div class="flex gap-2 mb-2">
+                    <a href="#" class="underline decoration-wavy">Terms of Service</a>•<a href="#" class="underline decoration-wavy">Privacy Policy</a>
+                </div>
+                <p>&copy; Copyright {{date('Y')}}</p>
+            </section>
+            <section class="flex justify-between">
+                <div class="text-slate-200">
+                    <h2 class="font-semibold .text-lg underline decoration-wavy mb-2">Company</h2>
+                    <ul class="flex flex-col gap-3">
+                        <li>About us</li>
+                        <li>Careers</li>
+                        <li>Contact us</li>
+                    </ul>
+                </div>
+                <div class="text-slate-200">
+                    <h2 class="font-semibold .text-lg underline decoration-wavy mb-2">Information</h2>
+                    <ul class="flex flex-col gap-3">
+                        <li>FAQ</li>
+                        <li>Blog</li>
+                        <li>Support</li>
+                    </ul>
+                </div>
+                <div class="text-slate-200">
+                    <h2 class="font-semibold .text-lg underline decoration-wavy mb-2">Community</h2>
+                    <ul class="flex flex-col gap-3">
+                        <li>Donations</li>
+                    </ul>
+                </div>
+            </section>
+</footer>
