b4c95e6eb21c89203994ddb9f0babb25b1a6e3af
Prince Asiedu <princekofasiedu@gmail.com>
2024-09-24 10:12:12 +0000

    style: generate link position edit

--- resources/views/livewire/generate-link.blade.php
+++ resources/views/livewire/generate-link.blade.php
@@ -1,4 +1,4 @@
-<div class="flex justify-center items-center">
+<div class="flex justify-center items-center h-[500px]">
     <section x-data = "{ copied: false }" class="bg-white w-[500px] py-10  rounded-md px-6 text-gray-600">
         <div class="flex items-center gap-4 .justify-center ">
             <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-link"><path d="M10 13a5 5 0 0 0 7.54.54l3-3a5 5 0 0 0-7.07-7.07l-1.72 1.71"/><path d="M14 11a5 5 0 0 0-7.54-.54l-3 3a5 5 0 0 0 7.07 7.07l1.71-1.71"/></svg>
