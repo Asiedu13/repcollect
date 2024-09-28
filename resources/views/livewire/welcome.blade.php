686ec9dea82a03b8260def3305c817ddeb891275
Prince Asiedu <princekofasiedu@gmail.com>
2024-09-28 18:25:09 +0000

    ui: homepage responsive

--- resources/views/livewire/welcome.blade.php
+++ resources/views/livewire/welcome.blade.php
@@ -3,9 +3,9 @@
         <section class="flex flex-col justify-between items-center w-[inherit] lg:h-[550px] lg:flex-row">
             <section class="flex flex-col justify-between w-[inherit] text-slate-600">
                 <h1 class="text-3xl font-medium text-gray-500">RepCollect</h1>
-                <p class="text-5xl font-bold my-2">Be Safe.</p>
-                <p class="text-5xl font-bold my-2">Be Organized.</p>
-                <p class="text-5xl font-bold my-2">Be Transparent.</p>
+                <p class="text-4xl font-bold my-2 lg:text-5xl">Be Safe.</p>
+                <p class="text-4xl font-bold my-2 lg:text-5xl">Be Organized.</p>
+                <p class="text-4xl font-bold my-2 lg:text-5xl">Be Transparent.</p>
                 <p class="mt-5">Start <b class="bg-green-400 text-white p-1 rounded-md ">tracking</b> payments and group collections today in three easy steps</p>
                 @guest
                     <section class='flex .justify-center items-center mt-[40px] gap-5'>
