7f1f3548d550519ddbda94de1843b81f0e168582
Prince Asiedu <princekofasiedu@gmail.com>
2024-09-28 16:56:58 +0000

    ui: make faqs page responsive

--- resources/views/livewire/dashboard-faq.blade.php
+++ resources/views/livewire/dashboard-faq.blade.php
@@ -1,9 +1,9 @@
- <main x-data="{qopen: false, success:false}" class="bg-white mt-5 rounded-md h-[fit] lg:w-[700px] flex flex-start flex-col .container px-2">
-            <header class="flex gap-2 .text-gray-600 text-sky-500 font-semibold items-center py-4 px-4">
+ <main x-data="{qopen: false, success:false}" class="bg-white mt-5 rounded-md h-[fit] lg:w-[700px] flex flex-start flex-col px-2">
+            <header class="flex gap-2 text-sky-500 font-semibold items-center py-4 px-4">
                 <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-message-circle-question">
                     <path d="M7.9 20A9 9 0 1 0 4 16.1L2 22Z"/><path d="M9.09 9a3 3 0 0 1 5.83 1c0 2-3 3-3 3"/><path d="M12 17h.01"/>
                 </svg>
-                <h2 class="text-xl border-r-2 border-sky-400 pr-4">Frequently Asked Questions</h2> 
+                <h2 class="text-sm lg:text-xl border-r-2 border-sky-400 pr-4">Frequently Asked Questions</h2> 
                 <button @click="qopen = true; success= false" href="#" class="flex gap-2 font-normal items-center text-sm text-sky-500">
                     <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-circle-plus"><circle cx="12" cy="12" r="10"/><path d="M8 12h8"/><path d="M12 8v8"/></svg>
                  Ask a question
