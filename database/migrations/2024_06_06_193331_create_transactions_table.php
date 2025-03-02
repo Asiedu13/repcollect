b9c5332e9e28f9f4553fdb4b8d23bbe49b6a3b6a
Prince Asiedu <princekofasiedu@gmail.com>
2025-03-02 20:30:22 +0000

    fix: focus -> Focus

--- database/migrations/2024_06_06_193331_create_transactions_table.php
+++ database/migrations/2024_06_06_193331_create_transactions_table.php
@@ -1,6 +1,6 @@
 <?php
 
-use App\Models\focus;
+use App\Models\Focus;
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
