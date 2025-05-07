c49bdc59e1efef3ece4ea65ee6bac597c721e757
Prince Asiedu <princekofasiedu@gmail.com>
2025-05-07 20:47:00 +0000

    landing: how it works section

--- resources/views/livewire/home.blade.php
+++ resources/views/livewire/home.blade.php
@@ -2,4 +2,5 @@
     {{-- To attain knowledge, add things every day; To attain wisdom, subtract things every day. --}}
     <livewire:marketing.hero></livewire:marketing.hero>
     <livewire:landing.features></livewire:landing.features>
+    <livewire:landing.how-it-works></livewire:landing.how-it-works>
 </div>
