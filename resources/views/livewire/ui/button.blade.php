b709155840c0022172361cde176626de25183c3c
Prince Asiedu <princekofasiedu@gmail.com>
2025-05-07 20:35:39 +0000

    remove unused files

--- resources/views/livewire/ui/button.blade.php
+++
@@ -1,6 +0,0 @@
-<div>
-    {{-- Care about people's approval and you will be their prisoner. --}}
-    <button class="inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0" {{$className}}>
-        Do something {{$slot}}
-    </button>
-</div>
